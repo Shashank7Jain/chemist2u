6d23b415c0a140f6b05d6c98e6cd003a48863053
Shashank7Jain <1509sj@gmail.com>
2021-04-20 13:04:01 +1000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chemist2u';
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit() {}
+
+  form: FormGroup = this.formBuilder.group({
+    name: [, { validators: [Validators.required], updateOn: 'change' }],
+    email: [
+      ,
+      {
+        validators: [Validators.required, Validators.email],
+        updateOn: 'change',
+      },
+    ],
+    text: [, { validators: [Validators.required], updateOn: 'change' }],
+  });
+  submitForm() {
+    console.log('hi');
+    console.log(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//reactive forms
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    NoopAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
